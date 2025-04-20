8db671c09fc9c4b42211566a4da7cea7ea70f9c9
hilarionga <hilarionga333@gmail.com>
2025-04-20 19:16:04 -0300

    hooks y visible

--- cypress/e2e/smoketestbank.cy.js
+++ cypress/e2e/smoketestbank.cy.js
@@ -1,13 +1,18 @@
 describe("test suite - conjunto de pruebas", ()=>{
-    it("Validar pagina de inicio", ()=>{
+    
+    beforeEach(()=>{
         cy.visit("http://zero.webappsecurity.com")
+    })
+    
+    it("Validar pagina de inicio", ()=>{
+        //cy.visit("http://zero.webappsecurity.com")
         cy.get(".active > img").should("be.visible")
         cy.get(".active > .custom > h4").contains("Online Banking")
         
     })
 
     it("Test E2E - transferencia de fondos", () =>{
-        cy.visit("http://zero.webappsecurity.com")
+        
         cy.get('#signin_button').click()
         cy.get('#user_login').type("username")
         cy.get('#user_password').type("password")
@@ -25,5 +30,20 @@ describe("test suite - conjunto de pruebas", ()=>{
         
     })
 
+    it.only("Prueba de actualizacion del grafico", () =>{
+       
+        cy.get('#signin_button').click()
+        cy.get('#user_login').type("username")
+        cy.get('#user_password').type("password")
+        cy.get('.btn').click()
+        cy.get('#money_map_tab > a').click()
+        cy.get('#ext-sprite-1259').should("be.visible")
+        cy.get('#ext-sprite-1167 > tspan').click()
+        cy.get('#ext-sprite-1259').should("not.be.visible")
+
+
+
+
+    })
     
 })
